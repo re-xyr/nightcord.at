ac66840ab027cbe68719a00b402ad64f3c1a8765
daylily <i@dayli.ly>
2025-09-08 00:55:58 -0400

    Add site title and metadata

--- @nightcord/public/src/routes/+layout.svelte
+++ @nightcord/public/src/routes/+layout.svelte
@@ -7,6 +7,14 @@ let { children } = $props()
 
 <svelte:head>
   <link rel="icon" href={favicon} />
+
+  <title>nightcord.at/25</title>
+
+  <meta name="description" content="「光って　光って　眠らない25時」" />
+
+  <meta property="og:image" content="/empty-sekai.png" />
+  <meta property="og:title" content="nightcord.at/25" />
+  <meta property="og:site_name" content="nightcord.at/25" />
 </svelte:head>
 
 {@render children?.()}
