09d77ea7385aae2c13fb8abed43cee220d07227a
daylily <i@dayli.ly>
2025-09-09 17:36:25 -0400

    Move favicon to public directory

--- @nightcord/public/src/routes/+layout.svelte
+++ @nightcord/public/src/routes/+layout.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 import '../app.css'
-import favicon from '$lib/assets/favicon.svg'
 import banner from '$lib/assets/empty-sekai.png'
 
 let { children } = $props()
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+  <link rel="icon" href="/favicon.svg" />
 
   <title>nightcord.at/25</title>
 
